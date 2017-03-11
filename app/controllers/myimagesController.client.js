1e1458fc7a2487d27c493de619d24ce82cc86bb2
John Clark <johngclark36@gmail.com>
2017-03-11 17:05:32 -0500

    add css styling to .info-div

--- app/controllers/myimagesController.client.js
+++ app/controllers/myimagesController.client.js
@@ -1,6 +1,42 @@
 
 $(document).ready(function () {
 
+
+    function addOrRemoveLike () {
+      var $span = $(this).next()
+      var spanVal = parseInt($span.text())
+      var imageId = $(this).data("imageId")
+      var userId = $(this).data("userId")
+
+      $.ajax({
+        url: '/images/' + imageId + '/likes',
+        method: 'get',
+        dataType: 'json'
+      })
+      .done(function (result) {
+        var likes = result.images[0].likes;
+        console.log(likes)
+        console.log(imageId)
+        if (likes.indexOf(userId) === -1) {
+
+          $.ajax({
+            url: '/images/' + imageId + '/addLike',
+            method: 'get'
+          }).done(function () {
+            $span.text(spanVal + 1)
+          })
+        } else {
+
+          $.ajax({
+            url: '/images/' + imageId + '/removeLike',
+            method: 'get'
+          }).done(function () {
+            $span.text(spanVal - 1)
+          })
+        }
+      })
+    }
+
   var $imagesGrid = $('.myimages');
   $.ajax({
     url: '/images',
@@ -22,6 +58,8 @@ $(document).ready(function () {
         var $deleteDiv = $('<div>', { 'class': 'delete' })
         $deleteDiv.data('id', image._id)
 
+
+
         $deleteDiv.on('click', function () {
           var parent = $(this).parent().parent()
           var imageId = $(this).data('id')
@@ -35,9 +73,18 @@ $(document).ready(function () {
 
         var $likesDiv = $('<div>', { 'class': 'likes'})
 
+        $likesDiv.data('imageId', image._id)
+        $likesDiv.data('userId', user.twitter.id)
+        $spanCounter = $('<span>', { 'class': 'likes-count', 'text': image.likes.length})
+
+
+        $likesDiv.on('click', addOrRemoveLike)
+
+
 
         $infoDiv.append($deleteDiv)
         $infoDiv.append($likesDiv)
+        $infoDiv.append($spanCounter)
         $div.append($img)
         $div.append($p)
         $div.append($infoDiv)
