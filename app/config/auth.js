724f50ef1651005f703e41270701f3e728228173
John Clark <johngclark36@gmail.com>
2017-03-13 09:52:26 -0400

    configure env variables for deployment

--- app/config/auth.js
+++ app/config/auth.js
@@ -4,12 +4,12 @@ module.exports = {
 	'githubAuth': {
 		'clientID': process.env.GITHUB_KEY,
 		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/github/callback'
+		'callbackURL': 'https://memeterest.herokuapp.com/' + 'auth/github/callback'
 	},
 
 	'twitterAuth' : {
         'consumerKey'       : process.env.TWITTER_KEY,
         'consumerSecret'    : process.env.TWITTER_SECRET,
-        'callbackURL'       : 'http://127.0.0.1:8080/auth/twitter/callback'
+        'callbackURL'       : 'https://memeterest.herokuapp.com/auth/twitter/callback'
     }
 };
